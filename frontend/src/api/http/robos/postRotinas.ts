fe6cc2d0ae88a769bba04bb71f014519cb9e6e4c
MatheusZanon <mzblannes@outlook.com>
2024-04-10 14:33:29 -0300

    refactor: terminando de adaptar chamadas de api com autenticacao

--- frontend/src/api/http/robos/postRotinas.ts
+++ frontend/src/api/http/robos/postRotinas.ts
@@ -3,6 +3,6 @@ import { CriarRotinaType } from '@/utils/types/criar_rotina';
 import { Parametro } from '@/utils/types/parametro';
 export async function postRotinas(roboId: number, data: CriarRotinaType) {
     const token = localStorage.getItem('accessToken');
-    const res = await api.post<Parametro>(`robos/${roboId}/rotinas/criar/`, {headers: {Authorization: `Bearer ${token}`}, data}).then((res) => res.data);
+    const res = await api.post<Parametro>(`robos/${roboId}/rotinas/criar/`, data, {headers: {Authorization: `Bearer ${token}`}}).then((res) => res.data);
     return res;
 }
