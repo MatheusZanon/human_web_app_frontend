177dfc11b4fec1b91a54c6be8bab95b00f2d2e33
Bruno Apolinário <bruno.apolinario010@gmail.com>
2024-04-24 14:38:49 -0300

    refactor: agora a tabela de economia formal pode ter um scroll, removido o arquivo json de testes utilizado na criação do componente de tabela

--- frontend/src/components/dashboard/economia-formal/table/index.tsx
+++ frontend/src/components/dashboard/economia-formal/table/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Table, TableBody, TableHeader, TableHead, TableData, TableRow, TableFooter } from '@/components/table';
 import type { getEconomiaFormal } from '@/utils/types/economia_formal';
+import styles from './economia_formal_table.module.scss';
 
 function EconomiaFormalTable({ data }: { data?: getEconomiaFormal[] }) {
     const [sortBy, setSortBy] = useState<keyof getEconomiaFormal>('nome_razao_social');
@@ -22,57 +23,59 @@ function EconomiaFormalTable({ data }: { data?: getEconomiaFormal[] }) {
 
     const somatorio = (values: number[]) => {
         return values.reduce((a, b) => a + b, 0).toFixed(2);
-    }
+    };
 
     return (
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableHeader
-                        columnKey='nome_razao_social'
-                        sortDirection={sortBy === 'nome_razao_social' ? sortDirection : ''}
-                        onSort={() => handleSort('nome_razao_social')}
-                        sortable
-                    >
-                        Empresa
-                    </TableHeader>
-                    <TableHeader
-                        columnKey='economia_formal'
-                        sortDirection={sortBy === 'economia_formal' ? sortDirection : ''}
-                        onSort={() => handleSort('economia_formal')}
-                        sortable
-                    >
-                        Economia Formal
-                    </TableHeader>
-                    <TableHeader
-                        columnKey='regiao'
-                        sortDirection={sortBy === 'regiao' ? sortDirection : ''}
-                        onSort={() => handleSort('regiao')}
-                        sortable
-                    >
-                        Região
-                    </TableHeader>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {sortedData.map((row, index) => (
-                    <TableRow key={`${row.nome_razao_social}_${index}`}>
-                        <TableData>{row.nome_razao_social}</TableData>
-                        <TableData>{row.economia_formal}</TableData>
-                        <TableData>{row.regiao}</TableData>
+        <div className={`w-100 d-block ${styles.table_container}`}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableHeader
+                            columnKey='nome_razao_social'
+                            sortDirection={sortBy === 'nome_razao_social' ? sortDirection : ''}
+                            onSort={() => handleSort('nome_razao_social')}
+                            sortable
+                        >
+                            Empresa
+                        </TableHeader>
+                        <TableHeader
+                            columnKey='economia_formal'
+                            sortDirection={sortBy === 'economia_formal' ? sortDirection : ''}
+                            onSort={() => handleSort('economia_formal')}
+                            sortable
+                        >
+                            Economia Formal
+                        </TableHeader>
+                        <TableHeader
+                            columnKey='regiao'
+                            sortDirection={sortBy === 'regiao' ? sortDirection : ''}
+                            onSort={() => handleSort('regiao')}
+                            sortable
+                        >
+                            Região
+                        </TableHeader>
                     </TableRow>
-                ))}
-            </TableBody>
-            <TableFooter>
-                <TableRow>
-                    <TableData>
-                        <span className='fw-bold'>Total:</span>
-                    </TableData>
-                    <TableData>{somatorio(sortedData.map((row) => row.economia_formal))}</TableData>
-                    <TableData></TableData>
-                </TableRow>
-            </TableFooter>
-        </Table>
+                </TableHead>
+                <TableBody>
+                    {sortedData.map((row, index) => (
+                        <TableRow key={`${row.nome_razao_social}_${index}`}>
+                            <TableData>{row.nome_razao_social}</TableData>
+                            <TableData>{row.economia_formal}</TableData>
+                            <TableData>{row.regiao}</TableData>
+                        </TableRow>
+                    ))}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableData>
+                            <span className='fw-bold'>Total:</span>
+                        </TableData>
+                        <TableData>{somatorio(sortedData.map((row) => row.economia_formal))}</TableData>
+                        <TableData></TableData>
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </div>
     );
 }
 
