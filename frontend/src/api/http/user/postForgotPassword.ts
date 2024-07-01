3d562995ee25e20059d28104aad1b1c8b001d1ba
MatheusZanon <mzblannes@outlook.com>
2024-07-01 13:38:29 -0300

    refactor: padronizando metodos forgot/reset password

--- frontend/src/api/http/user/postForgotPassword.ts
+++ frontend/src/api/http/user/postForgotPassword.ts
@@ -2,7 +2,7 @@ import { api } from '@/utils/axios';
 
 export async function postForgotPassword(email: string) {
     const data = await api
-        .post<{ message: string }>('user/forgot-password/', {
+        .post<{ message: string }>('user/forgot_password/', {
             email: email,
         })
         .then((res) => res.data);
--- frontend/src/api/http/user/postResetPassword.ts
+++ frontend/src/api/http/user/postResetPassword.ts
@@ -2,7 +2,7 @@ import { api } from '@/utils/axios';
 
 export async function postResetPassword(new_password: string, token: string) {
     const data = await api
-        .post<{ message: string }>(`user/reset-password/?token=${token}`, {
+        .post<{ message: string }>(`user/reset_password/?token=${token}`, {
             new_password: new_password,
         })
         .then((res) => res.data);
