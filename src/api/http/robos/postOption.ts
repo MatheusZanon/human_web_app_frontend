189d0c3b1d04eafee65ea9f06e19d49c762cd858
Bruno Apolinário <bruno.apolinario010@gmail.com>
2024-07-04 14:27:32 -0300

    removendo console log

--- src/api/http/robos/postOption.ts
+++ src/api/http/robos/postOption.ts
@@ -2,7 +2,6 @@ import { api } from '@/utils/axios';
 import { CriarOptionType } from '@/utils/types/robos/criar_option';
 import { SelectOption } from '@/utils/types/robos/option';
 export async function postOption(roboId: number, data: CriarOptionType) {
-    console.log(api.getUri() + `robos/${roboId}/parametros/criar-option/`, data);
     const res = await api.post<SelectOption>(`robos/${roboId}/parametros/criar-option/`, data).then((res) => res.data);
     return res;
 }
