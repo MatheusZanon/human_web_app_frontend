5708561383206ef1cc587465c2dd5aad84b24be3
MatheusZanon <mzblannes@outlook.com>
2024-07-25 10:32:51 -0300

    retirando variavel de ambiente para link do frontend no axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosError } from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const WEB_BASE_URL = import.meta.env.VITE_WEB_BASE_URL;
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
